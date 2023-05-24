77b99d3a2022407ee164e1f3cdfb1a530f46bb3c
nirodha <nirodhasandanuwan2002@gmail.com>
2023-05-24 12:56:50 +0530

    watch

---
+++ src/app/pages/watch/watch.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watch',
+  templateUrl: './watch.component.html',
+  styleUrls: ['./watch.component.css']
+})
+export class WatchComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+
+}
