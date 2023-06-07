6d9c150aea10b788f7833d503551a29f06386fd3
nirodha <nirodhasandanuwan2002@gmail.com>
2023-06-07 22:06:43 +0530

    home page changed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,19 +7,9 @@ import {Component, OnInit, Renderer2} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private renderer: Renderer2) { }
 
-  ngOnInit() {
-    const isMobile = this.detectMobile();
 
-    if (isMobile) {
-      this.renderer.addClass(document.body, 'portrait-mode');
-    } else {
-      this.renderer.removeClass(document.body, 'portrait-mode');
-    }
-  }
+  constructor() { }
 
-  detectMobile(): boolean {
-    return window.innerWidth < 376; // Adjust the threshold according to your requirements
-  }
+  ngOnInit() {}
 }
