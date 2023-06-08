31b5391f53ccb4dc11f6c4502a22197502c53739
nirodha <nirodhasandanuwan2002@gmail.com>
2023-06-08 16:41:38 +0530

    home page desingning

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,16 +7,6 @@ import {Component, OnInit, Renderer2} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  myInterval = 1000;
-  activeSlideIndex = 0;
-  slides: {image: string; text?: string}[] = [
-    {image: 'assets/money.jpg'},
-    {image: 'assets/card-2.jpg'},
-    {image: 'assets/exam1.png'},
-    {image: 'assets/timewasting.jpg'},
-    {image: 'assets/welcome.jpg'}
-  ];
-
   constructor() { }
 
   ngOnInit() {}
